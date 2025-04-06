5ddadd5d38a5b58701a16ce821ab672eed5792e0
Alexander Martin <alex@onthe.computer>
2025-04-06 01:11:21 -0400

    use rg on the prompt

---
+++ README.md
@@ -0,0 +1,4 @@
+### Bootstrap
+```
+./bootstrap.sh
+```
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,15 @@
 #! /bin/zsh
-
-cp .zshrc "${HOME}/.zshrc"
-cp .zshenv "${HOME}/.zshenv"
+cp zsh/.zshrc "${HOME}/.zshrc"
+cp zsh/.zshenv "${HOME}/.zshenv"
 
 local config_root="${HOME}/.config"
 mkdir -p $config_root
 
-cp .aliases $config_root/.aliases
+cp zsh/.aliases $config_root/.aliases
 
 declare -a folders=("brew" "gh" "prompt")
 for f in "${folders[@]}"; do
-    [ ! -d "${config_root}/${f}" ] && cp -R $f "${config_root}/${f}"
+  rsync -a "$f/" "${config_root}/${f}/"
 done
 
 mkdir -p "${HOME}/.warp/themes"
@@ -19,7 +18,10 @@ cp warp/almartin.yaml "${HOME}/.warp/themes/almartin.yaml"
 cp git/.gitconfig "${HOME}/.gitconfig"
 
 if ! command -v brew &>/dev/null; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+source "${HOME}/.zshenv"
+source "${HOME}/.zshrc"
 
 brew bundle
